fda8db95bbca35d62c8ed91c27a7a8bd66f8d62f
Articulate4tech <ogunniyima@gmail.com>
2024-09-30 12:50:05 -0700

    final commit

---
+++ addition.js
@@ -0,0 +1,9 @@
+// JavaScript Code: Adding two numbers
+function add(a, b) {
+    return a + b;
+    }
+    
+    let result = add(2, 3);
+    console.log(result);
+
+    //This is to test that code is commiting .
